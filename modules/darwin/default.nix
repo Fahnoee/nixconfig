01a4ea1309d055e04dd84c1222595163a5664f3b
Mikkel Martinus Fahnoee <mikkel120202@hotmail.com>
2025-09-13 23:13:01 +0200

    Enhance macOS system configuration in default.nix with improved trackpad settings and Finder preferences

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -13,8 +13,25 @@
   # More icebooks => https://github.com/ryan4yin/nix-darwin-kickstarter/blob/main/rich-demo/modules/system.nix
   system = {
     defaults = {
+      NSGlobalDomain = {
+        AppleShowAllExtensions = true;
+        ApplePressAndHoldEnabled = false;
+        InitialKeyRepeat = 15; # lower is faster
+        KeyRepeat = 2;         # lower is faster
+        NSAutomaticSpellingCorrectionEnabled = false;
+        NSAutomaticCapitalizationEnabled = false;
+        NSAutomaticQuoteSubstitutionEnabled = false;
+        NSAutomaticDashSubstitutionEnabled = false;
+        NSAutomaticPeriodSubstitutionEnabled = false;
+        "com.apple.trackpad.scaling" = 2.0; # Trackpad tracking speed
+      };
+
       dock = {
-        # autohide = true;
+        autohide = true;
+        show-recents = false;
+        mru-spaces = false; # don’t reorder spaces
+        tilesize = 56;
+        minimize-to-application = true;
         # customize Hot Corners
         wvous-tl-corner = 2; # top-left - Mission Control
         # wvous-tr-corner = 13; # top-right - Lock Screen
@@ -23,12 +40,28 @@
       };
 
       finder = {
+        AppleShowAllFiles = true;
         _FXShowPosixPathInTitle = true; # show full path in finder title
-        AppleShowAllExtensions = true; # show all file extensions
         FXEnableExtensionChangeWarning = false; # disable warning when changing file extension
         QuitMenuItem = true; # enable quit menu item
         ShowPathbar = true; # show path bar
         ShowStatusBar = true; # show status bar
+        FXPreferredViewStyle = "Nlsv"; # list view
+        # Use Home for new Finder windows
+        NewWindowTarget = "Home";
+        # Optionally set the explicit path (uncomment to use)
+        # NewWindowTargetPath = "file:///Users/mmf/";
+      };
+
+      screencapture = {
+        # NOTE: use an absolute path; ~ is not expanded by defaults
+        location = "/Users/mmf/Downloads/Screenshots";
+        type = "png";
+      };
+
+      trackpad = {
+        Clicking = true;            # tap to click
+        TrackpadThreeFingerDrag = true;
       };
     };
 
